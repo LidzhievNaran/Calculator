1512c32e6d333f6d7fb9b58697b31e1ca1ad689b
NaranLidzhiev <lidzhiev-03@mail.ru>
2022-01-04 19:24:07 +0300

    ;D

--- main.cpp
+++ main.cpp
@@ -1,64 +1,64 @@
 #include <iostream>
 
-enum tToken
-{
+enum tToken {
     PLUS, MINUS, MUL, DIV, LPAR, RPAR, NUMBER, END, ERROR
 };
 
 tToken aktToken;
 double TokenNumber;
 char *srcPos;
-tToken searchToken()
 
-{
+tToken searchToken() {
     aktToken = ERROR;
-    if (*srcPos==0)
-    {
+    if (*srcPos == 0) {
         aktToken = END;
-    }
-    else
-    {
-        switch (*srcPos)
-        {
-            case '(': aktToken=LPAR; break;
-            case ')': aktToken=RPAR; break;
-            case '*': aktToken=MUL; break;
-            case '/': aktToken=DIV; break;
-            case '+': aktToken=PLUS; break;
-            case '-': aktToken=MINUS; break;
+    } else {
+        switch (*srcPos) {
+            case '(':
+                aktToken = LPAR;
+                break;
+            case ')':
+                aktToken = RPAR;
+                break;
+            case '*':
+                aktToken = MUL;
+                break;
+            case '/':
+                aktToken = DIV;
+                break;
+            case '+':
+                aktToken = PLUS;
+                break;
+            case '-':
+                aktToken = MINUS;
+                break;
         }
-        if (*srcPos>='0'&& *srcPos<'9')
-        {
-            aktToken=NUMBER;
+        if (*srcPos >= '0' && *srcPos < '9') {
+            aktToken = NUMBER;
             TokenNumber = 0.0;
         }
-        while (*srcPos>='0' && *srcPos<'9')
-        {
+        while (*srcPos >= '0' && *srcPos < '9') {
             TokenNumber *= 10;
-            TokenNumber += *srcPos-'0';
+            TokenNumber += *srcPos - '0';
             srcPos++;
         }
-        if (aktToken != NUMBER)
-        {
+        if (aktToken != NUMBER) {
             srcPos++;
         }
     }
     return aktToken;
 }
-tToken Error(char *s)
 
-{
+tToken Error(char *s) {
     std::cerr << s << std::endl;
     return ERROR;
 }
 
 double PlusMinus();
-double Brackets()
 
-{
+double Brackets() {
     double Number;
-    switch(aktToken)
-    {
+    switch (aktToken) {
         case NUMBER:
             searchToken();
             return TokenNumber;
@@ -68,8 +68,7 @@ double Brackets()
         case LPAR:
             searchToken();
             Number = PlusMinus();
-            if (aktToken != RPAR)
-            {
+            if (aktToken != RPAR) {
                 return Error(") expected");
             }
             searchToken();
@@ -79,59 +78,49 @@ double Brackets()
     }
     return Error("primary expected");
 }
-double MulDiv()
 
-{
+double MulDiv() {
     double Number;
 
     Number = Brackets();
-    while (aktToken==MUL || aktToken==DIV)
-    {
-        if (aktToken==MUL)
-        {
+    while (aktToken == MUL || aktToken == DIV) {
+        if (aktToken == MUL) {
             searchToken();
             Number *= Brackets();
-        }
-        else if (aktToken==DIV)
-        {
+        } else if (aktToken == DIV) {
             searchToken();
             Number /= Brackets();
         }
     }
     return Number;
 }
-double PlusMinus()
 
-{
+double PlusMinus() {
     double Number;
 
     Number = MulDiv();
-    while (aktToken==PLUS || aktToken==MINUS)
-    {
-        if (aktToken==PLUS)
-        {
+    while (aktToken == PLUS || aktToken == MINUS) {
+        if (aktToken == PLUS) {
             searchToken();
             Number += MulDiv();
-        }
-        else if (aktToken==MINUS)
-        {
+        } else if (aktToken == MINUS) {
             searchToken();
             Number -= MulDiv();
         }
     }
     return Number;
 }
-double analysis(char *s)
-{
+
+double analysis(char *s) {
     srcPos = s;
     searchToken();
 
     return PlusMinus();
 }
-int main()
-{
-    char* demo;
-    std::cin>> demo;
+
+int main() {
+    char *demo;
+    std::cin >> demo;
     double Number = analysis(demo);
     std::cout << Number << std::endl;
     return 0;
